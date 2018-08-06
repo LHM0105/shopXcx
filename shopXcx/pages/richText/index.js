1553c31ffa41e159f71c13a4d53f4bdbcbbf5683
liuhuimin0105 <1528573672@qq.com>
2018-08-06 16:37:23 +0800

    try

---
+++ shopXcx/pages/richText/index.js
@@ -0,0 +1,75 @@
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+// pages/richText/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options)
+    util.request(api.IndexUrlBannerDetail).then(function (res) {
+      console.log('请求banner详情',res)
+      if (res.errno === 0) {
+        // data.hotGoods = res.data.hotGoodsList
+        // that.setData(data);
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
